5d7aba6ac11fa03d3f279530db32689d8e4b25be
Luc Fauvel <lfauvel@devolutions.net>
2024-11-04 10:49:50 -0500

    Added dap bindings

--- nvim/lua/plugins/dap/nvim-dap.lua
+++ nvim/lua/plugins/dap/nvim-dap.lua
@@ -5,8 +5,21 @@ return {
     'mfussenegger/nvim-dap',
   },
 	config = function()
+    local keymap = vim.keymap -- for conciseness
+    local opts = { silent = true }
+
     require('mason-nvim-dap').setup({
       ensure_installed = {'netcoredbg'}
     })
+
+    local dap = require('dap')
+
+    keymap.set("n", "<leader>b", dap.toggle_breakpoint, opts)
+    keymap.set("n", "<F5>", dap.continue, opts)
+    keymap.set("n", "<F10>", dap.step_over, opts)
+    keymap.set("n", "<F11>", dap.step_into, opts)
+    keymap.set("n", "<F12>", dap.step_out, opts)
+    keymap.set("n", "<leader>r", dap.repl.open, opts)
+    keymap.set("n", "<leader>rl", dap.run_last, opts)
   end
 }
--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -52,13 +52,13 @@ return {
         keymap.set("n", "<leader>bd", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
         opts.desc = "Show line diagnostics"
-        keymap.set("n", "<leader>D", vim.diagnostic.open_float, opts) -- show diagnostics for line
+        keymap.set("n", "<leader>ld", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
         opts.desc = "Go to previous diagnostic"
-        keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+        keymap.set("n", "[ld", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
 
         opts.desc = "Go to next diagnostic"
-        keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+        keymap.set("n", "]ld", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
         opts.desc = "Show documentation for what is under cursor"
         keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
