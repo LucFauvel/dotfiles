965fc13a42cb30d4b7c84234eeb80ebf77650912
Luc Fauvel <luc.fauvel@hotmail.com>
2025-05-13 20:14:32 -0400

    Add ghostty config

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s ~/git/dotfiles/tmux ~/.config/tmux
 "source ~/git/dotfiles/.zshrc" >> ~/.zshrc
 ln -s ~/git/dotfiles/nvim ~/.config/nvim 
+ln -s ~/git/dotfiles/ghostty/config ~/.config/ghostty/config
